30182fbb8a5d887945f3e2fef5a93c0162c13dc4
badou11 <mcabpn@gmail.com>
2021-01-27 15:32:37 +0000

    adding some basic file.

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import altair as alt
+import pydeck as pdk
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
